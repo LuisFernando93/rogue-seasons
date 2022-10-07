ce632cc48ef91318d3e76708c1f84aa7f7232cd7
Luis Fernando <luispasquinelli@hotmail.com>
2022-10-06 22:58:39 -0300

    Basic player dash mechanic implemented

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,13 @@ public class Player : MonoBehaviour
     private Vector2 movement;
     public float moveSpeed = 5f;
 
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float dashPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +25,34 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        
+        if (isDashing) return;
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) {
+            StartCoroutine(Dash());
+        } 
     }
 
     void FixedUpdate()
     {
+        if (isDashing) return;
         PlayerMovement();
     }
 
-    void PlayerMovement() {
+    private void PlayerMovement() {
         rb.MovePosition(rb.position + movement.normalized * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private IEnumerator Dash() {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(movement.x * dashPower, movement.y * dashPower);
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
