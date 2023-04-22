b62fb1ccaafbb2f41008ddaa3a9ff8c0c5d5239c
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2023-04-21 22:33:19 -0300

    SoundManager overhaul cancelled. Debug in A* disabled

--- Assets/Scripts/SoundManager/SoundManager.cs
+++ Assets/Scripts/SoundManager/SoundManager.cs
@@ -2,14 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class SoundManager {
-
-    public enum Sound { 
-
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    [SerializeField] private AudioSource _musicSource, _effectsSource;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        } else
+        {
+            Destroy(this);
+        }
     }
 
+    public void PlaySFX(AudioClip clip)
+    {
+        _effectsSource.PlayOneShot(clip);
+    }
 
-    public static void playSound() {
-        
+    public void PlayMusic(AudioClip clip)
+    {
+        _musicSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMasterVolume(float value)
+    {
+        AudioListener.volume = value;
     }
 }
