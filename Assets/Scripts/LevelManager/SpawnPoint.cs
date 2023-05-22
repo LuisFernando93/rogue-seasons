e6f8c4d1b70abf588fd41ad22b3b709c2d46c29e
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2023-05-22 02:56:08 -0300

    Enemies spawn randomly, depending on the values on the spawn points. New prefab Room1

--- Assets/Scripts/LevelManager/SpawnPoint.cs
+++ Assets/Scripts/LevelManager/SpawnPoint.cs
@@ -6,6 +6,8 @@ public class SpawnPoint : MonoBehaviour
 {
     [SerializeField] private int meleeChance, rangedChance, turretChance;
 
+    private EnemyType[] availableEnemies = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +23,34 @@ public class SpawnPoint : MonoBehaviour
         {
             turretChance = 0;
         }
+
+        if (meleeChance + rangedChance + turretChance > 0)
+        {
+            availableEnemies = new EnemyType[meleeChance + rangedChance + turretChance];
+            for(int i = 0; i < meleeChance; i++)
+            {
+                availableEnemies[i] = EnemyType.Melee;
+            }
+            for (int i = 0; i < rangedChance; i++)
+            {
+                availableEnemies[i + meleeChance] = EnemyType.Ranged;
+            }
+            for (int i = 0; i < turretChance; i++)
+            {
+                availableEnemies[i + meleeChance + rangedChance] = EnemyType.Turret;
+            }
+        }
     }
 
     public EnemyType enemySpawned()
     {
-        return EnemyType.Melee;
+        if(availableEnemies != null)
+        {
+            int rngEnemy = Random.Range(0, availableEnemies.Length);
+            return availableEnemies[rngEnemy];
+        } else
+        {
+            return EnemyType.Null;
+        }
     }
 }
