08819af6e40dc9b718bba5e86434873d8a30cf49
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2023-09-17 00:19:17 -0300

    Level Generator script added to Level Manager object,
    Level Generator is a script that uses Prim Algorithm to generate a map that connects all the rooms randomly.

--- Assets/_Scripts/LevelGenerator/Path.cs
+++ Assets/_Scripts/LevelGenerator/Path.cs
@@ -6,11 +6,29 @@ public class Path : MonoBehaviour
     private float Weight;
     [SerializeField] private int From;
     [SerializeField] private int To;
+    [SerializeField] private int Id;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         this.Weight = UnityEngine.Random.value;
     }
 
+    public float getWeight()
+    {
+        return this.Weight;
+    }
+
+    public int getFrom()
+    {
+        return this.From;
+    }
+
+    public int getTo()
+    {
+        return this.To;
+    }
+
+    public int getId()
+        { return this.Id; }
 }
