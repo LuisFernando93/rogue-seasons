d97f907373b0f6e291364067d5c8f78a9ad8e4af
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2023-10-22 19:17:33 -0300

    Buttons added to Pause Menu
    Setting in Pause Menu is non functional

--- Assets/_Scripts/UI/PauseMenu.cs
+++ Assets/_Scripts/UI/PauseMenu.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool gameIsPaused { get; private set; } = false;
-    [SerializeField] private GameObject _pauseMenuUI;
+    [SerializeField] private GameObject _pauseMenuContainer;
+    [SerializeField] private Slider _masterVolumeSlider, _musicVolumeSlider, _SFXVolumeSlider;
     [SerializeField] private AudioClip _buttonClick;
+
+    void Start()
+    {
+        //_masterVolumeSlider.value = 
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,14 +33,14 @@ public class PauseMenu : MonoBehaviour
 
     private void Resume()
     {
-        _pauseMenuUI.SetActive(false);
+        _pauseMenuContainer.SetActive(false);
         Time.timeScale = 1f;
         gameIsPaused = false;
     }
 
     private void Pause()
     {
-        _pauseMenuUI.SetActive(true);
+        _pauseMenuContainer.SetActive(true);
         Time.timeScale = 0f;
         gameIsPaused = true;
     }
@@ -43,13 +50,45 @@ public class PauseMenu : MonoBehaviour
         SoundManager.Instance.PlaySFX(_buttonClick);
     }
 
+    public void ResumeButton()
+    {
+        Resume();
+    }
+
     public void ExitLevelButton()
     {
+        Resume();
         SceneManager.LoadScene("Hub");
     }
 
-    public void ExitButton()
+    public void ExitGameButton()
     {
         Application.Quit();
+    }
+
+    public void AdjustVolume(string type)
+    {
+        switch (type)
+        {
+            case "master":
+                SoundManager.Instance.ChangeMasterVolume(_masterVolumeSlider.value);
+                break;
+            case "music":
+                SoundManager.Instance.ChangeMusicVolume(_musicVolumeSlider.value);
+                break;
+            case "SFX":
+                SoundManager.Instance.ChangeSFXVolume(_SFXVolumeSlider.value);
+                break;
+            default:
+                //String incompativel
+                break;
+        }
+    }
+
+    public void Save()
+    {
+        OptionsData data = new OptionsData(_masterVolumeSlider.value, _musicVolumeSlider.value, _SFXVolumeSlider.value, PlayerPrefs.GetString("language", "PTBR"));
+        SaveSystem.SaveOptions(data);
+        Debug.Log("Opcoes salvas");
     }
 }
