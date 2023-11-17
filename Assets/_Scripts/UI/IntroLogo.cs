34d539ebfa4cbd2678766d8263df1b3f2223e3e4
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2023-11-17 14:00:50 -0300

    Logo added to game. Player damage sfx added

--- Assets/_Scripts/UI/IntroLogo.cs
+++ Assets/_Scripts/UI/IntroLogo.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class IntroLogo : MonoBehaviour
 {
+    private void Update()
+    {
+        if(Input.anyKey)
+        {
+            ToMainMenu();
+        }
+    }
+
     public void ToMainMenu()
     {
         SceneManager.LoadScene("Main menu");
